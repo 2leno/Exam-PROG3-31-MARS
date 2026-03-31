901e45064550e25efccf8483de537e114656414f
Lucas Andrianina ANDRIAMANGA <hei.lucas.13@gmail.com>
2026-03-31 14:44:40 +0300

    init commit

---
+++ src/main/java/school/hei/examprog331mars/ExamProg331MarsApplication.java
@@ -0,0 +1,13 @@
+package school.hei.examprog331mars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamProg331MarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamProg331MarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/school/hei/examprog331mars/ExamProg331MarsApplicationTests.java
@@ -0,0 +1,13 @@
+package school.hei.examprog331mars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamProg331MarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
